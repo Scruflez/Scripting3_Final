e2203f7046a83ebb4c1aceb0033b9f2e84700914
Grizzly47 <kpgbc123@gmail.com>
2022-05-26 18:37:03 -0700

    Completed Car Steering

--- Assets/Scripting/Cart/SimpleCarController.cs
+++ Assets/Scripting/Cart/SimpleCarController.cs
@@ -2,23 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    // Is this wheel attached to motor?
+    public bool motor;
+    // Does this wheel apply steer angle?
+    public bool steering;
+}
+
 public class SimpleCarController : MonoBehaviour
 {
+    // Acceleration Input Name;
+    public string playerAcceleration;
+    // Steering Input Name
+    public string playerSteering;
     // Information about each individual axle
     public List<AxleInfo> axleInfos;
     // Maximum torque the motor can apply to wheel
     public float maxMotorTorque;
     // Maximum steer angle the wheel can have
-    public float maxSteeringAngle; 
+    public float maxSteeringAngle;
+
+
+    // Finds the corresponding visual wheel
+    // Correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
+    {
+        if (collider.transform.childCount == 0)
+        {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
+    }
+
 
     public void FixedUpdate()
     {
-        // Forwards/Backwards Input
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        // Steering
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        // Movement forward with input
+        float motor = maxMotorTorque * Input.GetAxis(playerAcceleration);
+        // Sideways steering with side inputs
+        float steering = maxSteeringAngle * Input.GetAxis(playerSteering);
 
-        // Adjust each wheel
+        // Adjusts the wheels to move the car
         foreach (AxleInfo axleInfo in axleInfos)
         {
             if (axleInfo.steering)
@@ -31,17 +67,8 @@ public class SimpleCarController : MonoBehaviour
                 axleInfo.leftWheel.motorTorque = motor;
                 axleInfo.rightWheel.motorTorque = motor;
             }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
     }
-}
-
-[System.Serializable]
-public class AxleInfo
-{
-    public WheelCollider leftWheel;
-    public WheelCollider rightWheel;
-    // Is this wheel attached to motor?
-    public bool motor;
-    // Does this wheel apply steer angle?
-    public bool steering;
 }
