46d850cd2565db475bbc5abe06560efe28199c34
sparkplugmedia-codex <65257609+sparkplugmedia-codex@users.noreply.github.com>
2022-06-09 12:31:57 -0700

    Editing Win Screen Prefab, fixing the timers

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,15 @@ using TMPro;
 
 public class PlayerUI : MonoBehaviour
 {
-    public TMP_Text currentTime;
+    public TMP_Text totalTime;
+    //public TMP_Text currentTime;
+    public TMP_Text prevLapTime;
+    public TMP_Text laps;
+    public TMP_Text lap1Time;
+    public TMP_Text lap2Time;
+    public TMP_Text lap3Time;
+    public Player player;
+    public GameObject playerEndTimeScreen;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +25,28 @@ public class PlayerUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        totalTime.text = AsRaceTime(player.totalLapTime);
+        //currentTime.text = AsRaceTime(player.lapTimes[0]);
+        laps.text = "Laps: " + (player.lapNumber + 1) + "/" + GameManager.totalLaps;
+
+        if (player.lapNumber > 0)
+        {
+            prevLapTime.text = AsRaceTime(player.lapTimes[player.lapNumber]);
+        }
+    }
+
+    public void SetPlayerEndTimeScreen()
+    {
+        playerEndTimeScreen.SetActive(true);
+        lap1Time.text = AsRaceTime(player.lapTimes[1]);
+        lap2Time.text = AsRaceTime(player.lapTimes[2]);
+        lap3Time.text = AsRaceTime(player.lapTimes[3]);
+
+    }
+
+    public static string AsRaceTime(float seconds)
+    {
+        TimeSpan timeSpan = TimeSpan.FromSeconds(seconds);
+        return timeSpan.ToString("mm':'ss':'ff");
     }
 }
